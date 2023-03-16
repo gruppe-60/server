34af2cc24194b63c30a9a773fd256bb88fc317eb
SebastianWolter1 <seb@techtakel.berlin>
2023-03-16 16:48:26 +0100

    cookies done

--- app.js
+++ app.js
@@ -2,25 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 const listeRoute = require("./routes/listeRoute.js");
 const authRoute = require("./routes/authRoute.js");
 
-
 const app = express();
-app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
+app.use(cors({credentials: true,  origin: 'http://localhost:5174'}));
 dotenv.config();
 app.use(express.json());
-app.use(cookieParser());
 app.use("/liste", listeRoute);
 app.use("/auth", authRoute);
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.send({
-    error: error,
-    status: error.status || 500,
-  });
-});
 
 
 const URI = `mongodb+srv://${process.env.DB_USER}:${process.env
